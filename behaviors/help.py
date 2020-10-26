d8258d5ac6c83ff25001181c4c23d0bef7fc91fa
jisana3 <nathan.ashlock@jbhunt.com>
2020-10-26 10:27:00 -0500

    hjfd

--- behaviors/help.py
+++ behaviors/help.py
@@ -2,5 +2,5 @@ import discord
 
 class Help:
     # make pretty
-    def send_help(self, message):
+    async def send_help(self, message):
         await message.channel.send('Thank u for choosing honkbot. Here is a list of words i understand: !bday, !bday next, honk. Try typing in some emoji names too... :)')
