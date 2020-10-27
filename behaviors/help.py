fa1e9298b0299ad706c78ceab83d3b4b87edd7fa
Noah Kindervag <noahk@protonmail.com>
2020-10-27 17:31:20 -0500

    Update help.py

--- behaviors/help.py
+++ behaviors/help.py
@@ -3,4 +3,4 @@ import discord
 class Help:
     # make pretty
     async def send_help(self, message):
-        await message.channel.send('Thank u for choosing honkbot. Here is a list of words i understand: !bday, !bday next, honk. Try typing in some emoji names too... :)')
+        await message.channel.send('Thank u for choosing honkbot. Here is a list of words i understand: ^bday, ^bday next, honk. Try typing in some emoji names too... :)')
