1e2ec61ac4cc56978acd31eafe0033cb3c185fe8
Noah Kindervag <noahk@protonmail.com>
2020-10-28 23:53:33 -0500

    Added emojis and streams

--- behaviors/emoji.py
+++ behaviors/emoji.py
@@ -13,7 +13,8 @@ class Emoji:
         {'name': 'kyle', 'code': 'SexyKyle:388117155176120320', 'response': None},
         {'name': 'society', 'code': ':imdajokababy:563930572813107200', 'response': None},
         {'name': 'haha', 'code': ':haha:525009386867916830', 'response': None},
-        {'name': 'zzz', 'code': ':kylesnooze:536758017169817612', 'response': None}]
+        {'name': 'zzz', 'code': ':kylesnooze:536758017169817612', 'response': None},
+        {'name': 'stars', 'code': ':reaugh:745328891857469470', 'response': 'LET\'S GO STARS!!'}]
 
     def get_emoji(self, msgContent):
         emojiData = next((x for x in self.emojis if x['name'] in msgContent), None)
--- behaviors/streams.py
+++ behaviors/streams.py
@@ -4,7 +4,8 @@ class Streams:
     streamBank = [
         {'name': 'wolf', 'link': 'https://explore.org/livecams/currently-live/wolf-cam-1'},
         {'name': 'bear', 'link': 'https://explore.org/livecams/brown-bears/brown-bear-salmon-cam-brooks-falls'},
-        {'name': 'jelly', 'link': 'https://explore.org/livecams/under-the-water/seajelly-cam'}
+        {'name': 'jelly', 'link': 'https://explore.org/livecams/under-the-water/seajelly-cam'},
+        {'name': 'lofi', 'link': 'https://www.youtube.com/watch?v=5qap5aO4i9A'}
     ]
 
     def get_stream(self, msgContent):
