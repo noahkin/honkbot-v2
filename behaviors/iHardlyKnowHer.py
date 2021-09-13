ece352c26e9c4e56ea6c7e72639487d826de60dc
Noah Kindervag <noahk@protonmail.com>
2021-09-13 12:14:01 -0500

    Update iHardlyKnowHer.py
    Fix indexing

--- behaviors/iHardlyKnowHer.py
+++ behaviors/iHardlyKnowHer.py
@@ -6,6 +6,6 @@ class IHardlyKnowHer:
     async def send(self, message, msgContent):
         words = msgContent.split()
         for word in words:
-            if word.lower()[-2] == 'er':
+            if word.lower()[-2:] == 'er':
                 response = self.build_response(word)
                 await message.channel.send(response)
