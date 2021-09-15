6bf7df2e65675807882c8bc7d729ee6b7796d8e2
jisana3 <nathan.ashlock@jbhunt.com>
2021-09-15 14:29:48 -0500

    exclusions list and unique words only

--- behaviors/iHardlyKnowHer.py
+++ behaviors/iHardlyKnowHer.py
@@ -1,11 +1,33 @@
 class IHardlyKnowHer:
+    global exclusions = [
+        "together",
+        "whatever",
+        "forever",
+        "ever",
+        "never",
+        "her",
+        "er",
+        "cancer",
+        "over",
+        "other",
+        "after",
+        "later",
+        "rather",
+        "sister",
+        "brother",
+        "father",
+        "mother",
+        "whether",
+        "either"
+        ]
+
     def build_response(self, word):
         word = word.captialize()
         return f"{word}? I hardly know her!"
-
+    
     async def send(self, message, msgContent):
-        words = msgContent.split()
+        words = set(msgContent.split())
         for word in words:
-            if word.lower()[-2] == 'er':
+            if word.lower()[-2] == 'er' and word not in exclusions:
                 response = self.build_response(word)
                 await message.channel.send(response)
